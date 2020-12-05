384f3fe7feeb0be3cc17539ca096d4bd50fba9e1
Ben Keener <keener4christ@gmail.com>
2020-12-05 01:34:23 -0500

    Imported integration and added project assets

---
+++ Assets/Scripts/InputHandler.cs
@@ -0,0 +1,80 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InputHandler : MonoBehaviour
+{
+    public GameObject rConObject;
+    public GameObject touchSphere;
+
+    public float maxDist = 100;
+
+    private Transform rConTransform;
+    private Transform touchSphereTransform;
+
+    private GameObject contact = null;
+    private GameObject lastContact = null;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rConTransform = rConObject.transform;
+        touchSphereTransform = touchSphere.transform;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        CheckContact();
+
+        if (OVRInput.GetDown(OVRInput.Button.SecondaryIndexTrigger))
+        {
+            if (contact != null)
+            {
+                PointerClick pc = contact.GetComponent<PointerClick>();
+
+                if (pc != null)
+                    pc.Click();
+            }
+        }
+    }
+
+    private void CheckContact() {
+        int layerMask = 1;
+
+        RaycastHit hit;
+        
+        lastContact = contact;
+
+        if (Physics.Raycast(rConTransform.position,
+                    rConTransform.TransformDirection(Vector3.forward),
+                    out hit, maxDist))
+        {
+            touchSphere.SetActive(true);
+            touchSphereTransform.position = hit.point;
+
+            contact = hit.transform.gameObject;
+
+            /*
+            Debug.Log(lastContact == null ? "null" : lastContact.name);
+            Debug.Log(contact == null ? "null" : contact.name);
+            Debug.Log("");
+            */
+        }
+        else
+        {
+            touchSphere.SetActive(false);
+            contact = null;
+        }
+
+        if (!GameObject.ReferenceEquals(contact, lastContact))
+        {
+            Debug.Log("Object point changed");
+            if (lastContact != null)
+                lastContact.GetComponent<PointerClick>().UnHover();
+
+            if (contact != null)
+                contact.GetComponent<PointerClick>().Hover();
+        }
+    }
+}
