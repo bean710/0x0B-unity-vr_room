14a7754f1e26542fc7734d7c3ee2364589a8fb98
Ben Keener <keener4christ@gmail.com>
2020-12-18 21:16:49 -0500

    Added keycode entry

---
+++ Assets/Scripts/KeypadButton.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class KeypadButton : MonoBehaviour
+{
+    public Transform leftHand;
+    public Transform rightHand;
+
+    public KeypadController kc;
+    public int number;
+
+    public float delay = 500;
+
+    private Bounds bounds;
+    private float lastTime;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        bounds = GetComponent<Collider>().bounds;
+        lastTime = Time.time;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Debug.Log($"TD: {Time.time - lastTime}");
+        if ((Time.time - lastTime) > (delay / 1000) && (bounds.Contains(leftHand.position) || bounds.Contains(rightHand.position)))
+        {
+            kc.EnterNumber(number);
+            lastTime = Time.time;
+        }
+    }
+}
---
+++ Assets/Scripts/KeypadController.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class KeypadController : MonoBehaviour
+{
+    public GameObject screenText;
+    public Animator glassDoor;
+    public string password = "314159";
+
+    private string entered = "000000";
+
+    private TextMeshPro tm;
+    private bool unlocked = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        tm = screenText.GetComponent<TextMeshPro>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (entered == password)
+        {
+            unlocked = true;
+            entered = "<color=\"green\">314159</color>";
+            tm.text = entered;
+            glassDoor.SetBool("character_nearby", true);
+        }
+        
+        if (!unlocked)
+            tm.text = entered;
+    }
+
+    public void EnterNumber(int num)
+    {
+        if (!unlocked)
+            entered = num.ToString() + entered.Substring(0, entered.Length - 1);
+    }
+}
