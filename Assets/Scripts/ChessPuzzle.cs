18ba201b7432a67a18433f33a940cc81a5c6ad49
Ben Keener <keener4christ@gmail.com>
2020-12-19 00:09:53 -0500

    Added chess puzzle logic

---
+++ Assets/Scripts/ChessPuzzle.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChessPuzzle : MonoBehaviour
+{
+    public List<Transform> pieces;
+    public List<Collider> boxes;
+
+    public GameObject projectorParticles;
+
+    private bool activated = false;
+    private ParticleSystem projPartSys;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        projPartSys = projectorParticles.GetComponent<ParticleSystem>();
+    }
+
+    public void CheckSolution()
+    {
+        if (activated)
+            return;
+
+        for (int i = 0; i < pieces.Count; i++)
+        {
+            if (!boxes[i].bounds.Contains(pieces[i].position))
+                return;
+        }
+
+        activated = true;
+        projectorParticles.SetActive(true);
+        projPartSys.Play();
+    }
+}
---
+++ Assets/Scripts/PieceTrigger.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PieceTrigger : MonoBehaviour
+{
+    public ChessPuzzle chessPuzzle;
+
+    private void OnCollisionEnter(Collision other) {
+        chessPuzzle.CheckSolution();
+    }
+}
